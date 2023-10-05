11f9846efdfca604c7a01658601ef3dab29d27ec
Goekhan Agkurt <g.agkurt@hotmail.de>
2023-10-05 15:30:03 +0200

    wq
    :wq
    :q
    :q

--- js/index.js
+++ js/index.js
@@ -5,9 +5,14 @@ const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
+  const toast = document.createElement("li");
+  toast.classList.add("toast-container__message");
+  toast.textContent="hello there";
+  toastContainer.append(toast);
   // Exercise: Append a new entry to the toast messages container
 });
 
 clearButton.addEventListener("click", () => {
+  toastContainer.innerHTML="";
   // Exercise: Clear the stack of toast messages
 });
